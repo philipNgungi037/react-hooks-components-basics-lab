8513a88f13ec73bc77b0a9571a8065114ecdb2ae
PHILIP <philophilo037@gmail.com>
2022-09-21 00:51:27 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,14 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+// {/* write an <About> component here */}
+function About(){
+  return(
+    <div id="#about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
@@ -25,6 +32,11 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+        <Home />
+        <About />
+
+
+
     </div>
   );
 }
